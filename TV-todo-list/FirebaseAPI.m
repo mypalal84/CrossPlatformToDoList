02621f9e1fbd50bd2c537d84891a59deabb81b5a
Alex Cahn <alexcahn@gmail.com>
2017-05-10 10:45:42 -0700

    put credentials into gitignore

---
+++ TV-todo-list/FirebaseAPI.m
@@ -0,0 +1,13 @@
+//
+//  FirebaseAPI.m
+//  CrossPlatformToDoList
+//
+//  Created by A Cahn on 5/10/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import "FirebaseAPI.h"
+
+@implementation FirebaseAPI
+
+@end
