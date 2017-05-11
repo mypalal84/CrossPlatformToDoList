fb7fd30ad71162d9281ce30e8ebe0db7057fbe5e
Alex Cahn <alexcahn@gmail.com>
2017-05-10 17:20:53 -0700

    got delet function to work

---
+++ CrossPlatformToDoList/CompletedViewController.m
@@ -0,0 +1,37 @@
+//
+//  CompletedViewController.m
+//  CrossPlatformToDoList
+//
+//  Created by A Cahn on 5/10/17.
+//  Copyright © 2017 A Cahn. All rights reserved.
+//
+
+#import "CompletedViewController.h"
+
+@interface CompletedViewController ()
+
+@end
+
+@implementation CompletedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
